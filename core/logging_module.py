7c25414e179004ad8c626be359832b440c811006
Ptuttle91 <patuttle2.0@gmail.com>
2026-02-19 14:36:19 -0800

    Update logging_module.py

--- core/logging_module.py
+++ core/logging_module.py
@@ -1,1 +1,21 @@
+# This module serves to define and shape user I/O as it will appear in the history log and exports.
+# See ReadMe.md for references used.
 
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class Logging:
+    timestamp: str
+    task_name: str
+    inputs: dict
+    outputs: str
+
+    @staticmethod
+    def create(task_name: str, inputs:dict, outputs) -> "LogEntry":
+        return LogEntry(
+            timestamp=datetime.now(),
+            task_name=task_name,
+            inputs=inputs,
+            outputs=str(outputs)
+        )
