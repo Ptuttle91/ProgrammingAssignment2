b51eebf4e6f976043dbaa5b7187bc1661f596af9
Ptuttle91 <patuttle2.0@gmail.com>
2026-02-19 15:48:46 -0800

    Update task_registry.py
    Created file description, file outline

--- core/task_registry.py
+++ core/task_registry.py
@@ -1,1 +1,7 @@
-
+#This module serves as a central registry to describe each task in a way that will be friendly to the GUI
+#It will:
+# - Keep the GUI decoupled
+# - Define what inputs each task expects so suitable fields can be rendered in the GUI
+# - Allow expansion or new entries should the need arise
+# How?: 1) GUI will load tasks. 2) User selects task.  3) GUI renders field from input. 4) GUI resolves function with call.
+# See ReadMe.md for references used.
